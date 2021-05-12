77e17084672953617f67f7a1e6ae4ad2b492230e
Renaud17 <45036260+Renaud17@users.noreply.github.com>
2021-05-12 18:12:51 +0100

    Update app1.py

--- app1.py
+++ app1.py
@@ -74,25 +74,6 @@ def main():
 				task = st.selectbox("Task",["Add Post","Analytics","Profiles"])
 				if task == "Enregistrer mom entreprise":
 					st.subheader("Add Your Article")
-					create_table()
-					blog_RC = st.text_input('Enter Notes RC')
-					blog_Société = st.text_input("Enter Société",max_chars=50)
-					blog_Secteur = st.text_input('Enter Notes Secteur')
-					blog_Activités = st.text_area("Enter Your Activités",height=200)
-					blog_Adresse = st.text_input("Enter Adresse",max_chars=50)
-					blog_Téléphone = st.text_input("Enter Téléphone",max_chars=50)
-					blog_Région = st.text_input("Enter Région",max_chars=50)
-
-					if st.button("Add"):
-					    import geocoder
-					    myloc = geocoder.ip('me')
-					    my=myloc.latlng
-					    blog_Longitude= my[1]
-					    blog_Latitude = my[0]
-					    add_data(blog_RC,blog_Société,blog_Secteur,blog_Activités,blog_Adresse,blog_Téléphone,blog_Région,blog_Latitude,blog_Longitude)
-					    st.success("Post::'{}' Saved".format(blog_RC))
-					
-					
 
 				elif task == "Analytics":
 					st.subheader("Analytics")
