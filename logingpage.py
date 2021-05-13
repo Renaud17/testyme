9bc4c35536d1e422d0fb2aa5fcf187e36a665e77
Renaud17 <45036260+Renaud17@users.noreply.github.com>
2021-05-13 11:46:13 +0100

    Create logingpage.py

---
+++ logingpage.py
@@ -0,0 +1,43 @@
+"""Simple Login App"""
+
+	st.title("Simple Login App")
+
+	menu = ["Login","SignUp"]
+	choice = st.sidebar.selectbox("Menu",menu)
+
+	elif choice == "Login":
+		st.subheader("Login Section")
+
+		username = st.sidebar.text_input("User Name")
+		password = st.sidebar.text_input("Password",type='password')
+		if st.sidebar.checkbox("Login"):
+			# if password == '12345':
+			create_usertable()
+			hashed_pswd = make_hashes(password)
+      
+			result = login_user(username,check_hashes(password,hashed_pswd))
+			if result:
+
+				st.success("Logged In as {}".format(username))
+       
+				elif task == "Entreprises":
+					st.subheader("Profile entreprises")
+				elif task == "Profiles":
+					st.subheader("User Profiles")
+					user_result = view_all_users()
+					clean_db = pd.DataFrame(user_result,columns=["Username","Password"])
+					st.dataframe(clean_db)
+			else:
+				st.warning("Incorrect Username/Password")
+
+
+	elif choice == "SignUp":
+		st.subheader("Create New Account")
+		new_user = st.text_input("Username")
+		new_password = st.text_input("Password",type='password')
+
+		if st.button("Signup"):
+			create_usertable()
+			add_userdata(new_user,make_hashes(new_password))
+			st.success("You have successfully created a valid Account")
+			st.info("Go to Login Menu to login")
