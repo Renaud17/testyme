7d6c07b603e309713792c42d60ce622c42be46cf
Renaud17 <45036260+Renaud17@users.noreply.github.com>
2021-05-13 15:58:59 +0100

    Update app1.py

--- app1.py
+++ app1.py
@@ -3,7 +3,9 @@ import fxc
 import streamlit as st
 import pandas as pd 
 import geocoder
-
+import base64
+from PIL import Image
+import io 
 
 import sqlite3
 conn = sqlite3.connect('data.db')
@@ -232,12 +234,14 @@ def main():
 				blog_Téléphone = st.text_input("Enter Téléphone",max_chars=50)
 				blog_Région = st.text_input("Enter Région",max_chars=50)
 				blog_image = st.file_uploader("Upload Image",type=['png','jpeg','jpg'])
+				file = open(blog_image,'rb').read()
+				file = base64.b64encode(file)
 				if st.button("Add"):
 				    import requests
 				    response = requests.get("http://ip-api.com/json/").json()
 				    blog_Longitude= response['lon']
 				    blog_Latitude = response['lat']
-				    add_data(blog_RC,blog_Société,blog_Secteur,blog_Activités,blog_Adresse,blog_Téléphone,blog_Région,blog_image,blog_Latitude,blog_Longitude)
+				    add_data(blog_RC,blog_Société,blog_Secteur,blog_Activités,blog_Adresse,blog_Téléphone,blog_Région,file,blog_Latitude,blog_Longitude)
 				    st.success("Post::'{}' Saved".format(blog_RC))
 				
 			else:
