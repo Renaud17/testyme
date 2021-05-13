d7fc558e1fa264dd0ea64e084a6fa2a89c3e552b
Renaud17 <45036260+Renaud17@users.noreply.github.com>
2021-05-13 13:34:11 +0100

    Update app1.py

--- app1.py
+++ app1.py
@@ -221,34 +221,23 @@ def main():
 			if result:
 
 				st.success("Logged In as {}".format(username))
-
-				task = st.selectbox("Task",["Enregistrement","Entreprises","Profiles"])
-				if task == "Enregistrement":
-					st.subheader("Votre enregistrement")
-					create_table()
-					blog_RC = st.text_input('Enter Notes RC')
-					blog_Société = st.text_input("Enter Société",max_chars=50)
-					blog_Secteur = st.text_input('Enter Notes Secteur')
-					blog_Activités = st.text_area("Enter Your Activités",height=200)
-					blog_Adresse = st.text_input("Enter Adresse",max_chars=50)
-					blog_Téléphone = st.text_input("Enter Téléphone",max_chars=50)
-					blog_Région = st.text_input("Enter Région",max_chars=50)
-					if st.button("Add"):
-					    import requests
-					    response = requests.get("http://ip-api.com/json/").json()
-					    blog_Longitude= response['lon']
-					    blog_Latitude = response['lat']
-					    add_data(blog_RC,blog_Société,blog_Secteur,blog_Activités,blog_Adresse,blog_Téléphone,blog_Région,blog_Latitude,blog_Longitude)
-					    st.success("Post::'{}' Saved".format(blog_RC))
-					
-                 
-				elif task == "Entreprises":
-					st.subheader("Profile entreprises")
-				elif task == "Profiles":
-					st.subheader("User Profiles")
-					user_result = view_all_users()
-					clean_db = pd.DataFrame(user_result,columns=["Username","Password"])
-					st.dataframe(clean_db)
+				st.subheader("Votre enregistrement")
+				create_table()
+				blog_RC = st.text_input('Enter Notes RC')
+				blog_Société = st.text_input("Enter Société",max_chars=50)
+				blog_Secteur = st.text_input('Enter Notes Secteur')
+				blog_Activités = st.text_area("Enter Your Activités",height=200)
+				blog_Adresse = st.text_input("Enter Adresse",max_chars=50)
+				blog_Téléphone = st.text_input("Enter Téléphone",max_chars=50)
+				blog_Région = st.text_input("Enter Région",max_chars=50)
+				if st.button("Add"):
+				    import requests
+				    response = requests.get("http://ip-api.com/json/").json()
+				    blog_Longitude= response['lon']
+				    blog_Latitude = response['lat']
+				    add_data(blog_RC,blog_Société,blog_Secteur,blog_Activités,blog_Adresse,blog_Téléphone,blog_Région,blog_Latitude,blog_Longitude)
+				    st.success("Post::'{}' Saved".format(blog_RC))
+				
 			else:
 				st.warning("Incorrect Username/Password")
 
