c0c57eb44fdb5a74986c9adf4e1e349f4d656ff0
Renaud17 <45036260+Renaud17@users.noreply.github.com>
2021-05-13 10:50:25 +0100

    Update app1.py

--- app1.py
+++ app1.py
@@ -6,11 +6,11 @@ import fxc
 
 st.beta_container()
 st.beta_columns(spec)
-    col1, col2 = st.beta_columns(2)
-    col1.subheader('Columnisation')
+col1, col2 = st.beta_columns(2)
+col1.subheader('Columnisation')
 st.beta_expander('Expander')
-    with st.beta_expander('Expand'):
-        st.write('Juicy deets')
+with st.beta_expander('Expand'):
+	st.write('Juicy deets')
 
 def main():
 	"""Simple Login App"""
